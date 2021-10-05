40b4544eab349a977cc32354e356f5affd241e57
Fernando Moncayo Márquez <71843000+FerchoSMT@users.noreply.github.com>
2021-10-05 18:11:41 -0500

    avance

--- app/src/main/java/com/example/pantalla_registro/MainActivity.kt
+++ app/src/main/java/com/example/pantalla_registro/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pantalla_registro
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar;
+    private lateinit var drawer: DrawerLayout;
+    private lateinit var toggle: ActionBarDrawerToggle;
+    private lateinit var activity:Activity;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro_usuario)
@@ -15,6 +28,21 @@ class MainActivity : AppCompatActivity() {
         val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
         spinner.adapter =adaptador
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer= findViewById(R.id.drawer_layout);
+        toggle = ActionBarDrawerToggle(activity,drawer,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    override fun onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed();
+        }
 
     }
+
 }
---
+++ app/src/main/java/com/example/pantalla_registro/MessageFragment.kt
@@ -0,0 +1,6 @@
+package com.example.pantalla_registro
+
+import android.support.v4.app.Fragment;
+
+class MessageFragment {
+}
