43a48033511e25c3ebde0cd296c700ebefa5eb75
Fernando Moncayo Márquez <71843000+FerchoSMT@users.noreply.github.com>
2021-10-10 21:40:24 -0500

    cambio

--- app/src/main/java/com/example/pantalla_registro/LoginActivity.kt
+++ app/src/main/java/com/example/pantalla_registro/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.example.pantalla_registro
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pantalla_registro.R
 import com.example.pantalla_registro.activities.ListOfChatsActivity
